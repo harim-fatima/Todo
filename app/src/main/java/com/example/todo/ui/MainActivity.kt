9cd5ba4f671399a5c9416f7a91ef56167f8ea468
harim-fatima <harim.muzamil@gmail.com>
2025-03-04 04:13:45 +0200

    using static data

---
+++ app/src/main/java/com/example/todo/ui/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.todo.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.todo.ui.theme.TodoTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.todo.viewmodel.TodoViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TodoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TodoScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun TodoScreen(modifier: Modifier = Modifier, todoViewModel: TodoViewModel =
+    viewModel()) {
+    TodoList(modifier,todoViewModel.todos)
+}
+@Composable
+fun TodoList(modifier: Modifier = Modifier, todos: List<String>) {
+    LazyColumn(
+        modifier = modifier
+    )
+    {
+        items(todos) { todo ->
+            Text(
+                text = todo,
+                modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+            )
+            HorizontalDivider(color = Color.LightGray, thickness = 2.dp)
+        }
+    }
+}
