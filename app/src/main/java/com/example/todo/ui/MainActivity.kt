8ebecf5ed92ad042942c0db85e900e9a4a38e4a0
harim-fatima <harim.muzamil@gmail.com>
2025-03-04 04:42:34 +0200

    getting data from internet api call

--- app/src/main/java/com/example/todo/ui/MainActivity.kt
+++ app/src/main/java/com/example/todo/ui/MainActivity.kt
@@ -7,16 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.todo.ui.theme.TodoTheme
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.todo.model.Todo
 import com.example.todo.viewmodel.TodoViewModel
 
 class MainActivity : ComponentActivity() {
@@ -42,14 +43,14 @@ fun TodoScreen(modifier: Modifier = Modifier, todoViewModel: TodoViewModel =
     TodoList(modifier,todoViewModel.todos)
 }
 @Composable
-fun TodoList(modifier: Modifier = Modifier, todos: List<String>) {
+fun TodoList(modifier: Modifier = Modifier, todos: List<Todo>) {
     LazyColumn(
         modifier = modifier
     )
     {
         items(todos) { todo ->
             Text(
-                text = todo,
+                text = todo.title,
                 modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
             )
             HorizontalDivider(color = Color.LightGray, thickness = 2.dp)
